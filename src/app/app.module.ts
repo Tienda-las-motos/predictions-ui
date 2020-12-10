395a83778c799e873b6ccee7cbb4a90478926297
Jorge Gu7man <46007655+jgu7man@users.noreply.github.com>
2020-12-10 13:22:49 -0600

    project initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FirebaseModule } from './firebase.module';
+import { ComunesModule } from './comunes.module';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FirebaseModule,
+    ComunesModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
